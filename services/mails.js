8438c1eced639a3bc30839366cabb6f064a62299
Romina Areco <romina.areco@infocorpgroup.com>
2023-06-10 12:11:17 -0300

    new apiKey

--- services/mails.js
+++ services/mails.js
@@ -4,11 +4,11 @@ async function sendEmailCreatedClient(client) {
     const { email } = client;
 
     const sgMail = require('@sendgrid/mail')
-    sgMail.setApiKey('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')//Take from config
+    sgMail.setApiKey('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')//TODO: Take from config
     
     const msg = {
         to: email, 
-        from: 'romiareco@gmail.com', // Change to your verified sender
+        from: 'romiareco@gmail.com', //TODO: Change to your verified sender
         subject: 'Welcome to WareUp',
         template_id: 'c4047597-e073-4b35-ba93-2c9724a3d1e5'
     }
