801a5e64d07da4f4b128322469627ff77f4f323f
letipresa <leticia.presa19@gmail.com>
2023-06-17 20:27:25 -0300

    Middleware added in order to control access token for api resources

---
+++ src/middleware/authenticated.js
@@ -0,0 +1,27 @@
+const jwt = require("../utils/jwt")
+ 
+function validateAuth(req, res, next) {
+    if(!req.headers.authorization) {
+        res.status(402).send({msg: "Usuario no autorizado"});
+    }
+    
+    const token = req.headers.authorization.replace("Bearer ", "").trim();
+    try {
+        const payload = jwt.decoded(token);
+        const { exp } = payload;
+        const currentDate = new Date().getDate();
+
+        if (exp <= currentDate) {
+            return res.status(400).send({msg: "Token expirado."});
+        }
+
+        req.user = payload;
+        next();
+    } catch (error) {
+        return res.status(400).send({msg: "Token inválido"});
+    }
+}
+
+module.exports = {
+    validateAuth
+}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const UserController = require("../controllers/user.controller");
 const AuthController = require("../controllers/auth.controller");
 const { userService } = require("./dependency");
+const mdAuth = require("../middleware/authenticated");
 
 const userController = new UserController(userService);
 
-router.post("/user", (req, res) => userController.register(req, res));
+router.post("/user", [mdAuth.validateAuth], (req, res) => userController.register(req, res));
 router.post("/user/recover-password", (req, res) => userController.recoverPassword(req, res));
 router.get("/user/:id", (req, res) => userController.getUser(req, res));
 router.get("/user", (req, res) => userController.getUsers(req, res));
