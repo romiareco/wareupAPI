fbaa1b1801ae0394eb34e6f934135fee34e51777
Romina Areco <romina.areco@infocorpgroup.com>
2023-07-11 20:28:06 -0300

    Fixes

--- src/routes/company.route.js
+++ src/routes/company.route.js
@@ -8,7 +8,7 @@ const companyController = new CompanyController(companyService);
 
 router.post("/companies", (req, res) => companyController.register(req, res)); 
 router.get("/companies", (req, res) => companyController.getAll(req, res)); 
-router.get("/companies/:id",/* [mdAuth.validateAuth],  */(req, res) => companyController.get(req, res));
-router.get("/companies/byUser/:id", /*[mdAuth.validateAuth], */(req, res) => companyController.getByUser(req, res));
+router.get("/companies/:id", [mdAuth.validateAuth],  (req, res) => companyController.get(req, res));
+router.get("/companies/byUser/:id", [mdAuth.validateAuth], (req, res) => companyController.getByUser(req, res));
 
 module.exports = router;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -9,8 +9,8 @@ const userController = new UserController(userService);
 
 router.post("/users", (req, res) => userController.register(req, res));
 router.post("/users/recover-password", (req, res) => userController.recoverPassword(req, res));
-router.get("/users/me", /*[mdAuth.validateAuth], */(req, res) => userController.getMe(req, res));
-router.get("/users/:id", /*[mdAuth.validateAuth],*/ (req, res) => userController.getUser(req, res));
-router.get("/users", /*[mdAuth.validateAuth], */(req, res) => userController.getUsers(req, res));
+router.get("/users/me", [mdAuth.validateAuth], (req, res) => userController.getMe(req, res));
+router.get("/users/:id", [mdAuth.validateAuth], (req, res) => userController.getUser(req, res));
+router.get("/users", [mdAuth.validateAuth], (req, res) => userController.getUsers(req, res));
 
 module.exports = router;
