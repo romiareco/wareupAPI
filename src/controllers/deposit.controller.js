d830556a0cdda30f94bec05a6144f1397b6ad8a9
Romina Areco <romina.areco@hotmail.com>
2023-08-21 15:34:21 -0300

    Fixes

--- src/controllers/deposit.controller.js
+++ src/controllers/deposit.controller.js
@@ -60,7 +60,7 @@ class DepositController {
       const { images } = req.body;
       const { id } = req.params;
 
-      if (!depositId || typeof depositId !== "number") {
+      if (!id || typeof id !== "number") {
         return res.status(400).json({ message: "Invalid Params" });
       }
       const result = await this.service.addDepositImages(id, images);
