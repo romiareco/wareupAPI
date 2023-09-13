81cce2a7ef85a9d20e8cb8b71d65bb188cfe039d
Romina Areco <romina.areco@hotmail.com>
2023-09-12 21:35:23 -0300

    UnitTest

--- src/services/bookingRequest.service.test.js
+++ src/services/bookingRequest.service.test.js
@@ -1,51 +1,12 @@
 const chai = require("chai");
 const sinon = require("sinon");
-const CompanyRepository = require("../repositories/company.repository");
+const BookingRequestRepository = require("../repositories/bookingRequest.repository");
 const expect = chai.expect;
 const LogRepository = require("../repositories/log.repository");
-const CompanyService = require("./company.service");
+const BookingRequestService = require("./bookingRequest.service");
 const { logRepository, userRepository} = require("../routes/dependency");
 
-describe("CompanyService", function() {
-
-  describe("delete", function() {
-
-    const stubValue = {
-      id: 1,
-      companyId: 1,
-      RUT: "200",
-      email: "asdadsa"
-    };
-
-    it("should delete a company", async function() {
-      sinon.restore();
-      const repository = new CompanyRepository();
-      sinon.stub(repository, "update").returns(stubValue);
-      sinon.stub(repository, "get").returns(stubValue);
-      
-      const service = new CompanyService(repository, logRepository, userRepository);
-      const result = await service.delete(stubValue);
- 
-      expect(result.hasError).to.equal(false); 
-      expect(result.company.id).to.equal(stubValue.id);
-    });
-
-    it("should return an error", async function() {
-      sinon.restore();
-      const logRepository = new LogRepository();
-      const repository = new CompanyRepository();
-      var stub = sinon.stub(repository, "update").throwsException(); 
-      var stubGeUser = sinon.stub(repository, "get").returns({ id: 1, });
-      stub = sinon.stub(logRepository, "create").returns();
-      //sinon.stub(repository, "getByRUT").returns(null);
-      const service = new CompanyService(repository, logRepository, userRepository);
-      const result = await service.delete(stubValue.id);
-
-      expect(stub.calledOnce).to.be.true;
-      expect(result.hasError).to.equal(true);
-    });
-  });
-
+describe("BookingRequestService", function() {
 
   describe("update", function() {
 
@@ -56,28 +17,28 @@ describe("CompanyService", function() {
       email: "asdadsa"
     };
 
-    it("should update a company", async function() {
+    it("should update a bookingRequest", async function() {
       sinon.restore();
-      const repository = new CompanyRepository();
+      const repository = new BookingRequestRepository();
       sinon.stub(repository, "update").returns(stubValue);
       sinon.stub(repository, "get").returns(stubValue);
       
-      const service = new CompanyService(repository, logRepository, userRepository);
+      const service = new BookingRequestService(repository, logRepository, userRepository);
       const result = await service.update(stubValue);
  
       expect(result.hasError).to.equal(false); 
-      expect(result.company.id).to.equal(stubValue.id);
+      expect(result.bookingRequest.id).to.equal(stubValue.id);
     });
 
     it("should return an error", async function() {
       sinon.restore();
       const logRepository = new LogRepository();
-      const repository = new CompanyRepository();
+      const repository = new BookingRequestRepository();
       var stub = sinon.stub(repository, "update").throwsException(); 
       var stubGeUser = sinon.stub(userRepository, "get").returns({ id: 1, });
       stub = sinon.stub(logRepository, "create").returns();
       //sinon.stub(repository, "getByRUT").returns(null);
-      const service = new CompanyService(repository, logRepository, userRepository);
+      const service = new BookingRequestService(repository, logRepository, userRepository);
       const result = await service.update(stubValue.id);
 
       expect(stub.calledOnce).to.be.true;
@@ -93,31 +54,30 @@ describe("CompanyService", function() {
       RUT: "200"
     };
 
-    it("should create a company", async function() {
+    it("should create a bookingRequest", async function() {
       sinon.restore();
-      const repository = new CompanyRepository();
+      const repository = new BookingRequestRepository();
       var stubCreate = sinon.stub(repository, "create").returns(stubValue);
       var stubGeUser = sinon.stub(userRepository, "get").returns({ id: 1, });
-      sinon.stub(repository, "getByRUT").returns(null);
       
-      const service = new CompanyService(repository, logRepository, userRepository);
+      const service = new BookingRequestService(repository, logRepository, userRepository);
       const result = await service.create(stubValue);
  
       
       expect(result.hasError).to.equal(false)
       expect(stubCreate.calledOnce).to.be.true;
-      expect(result.company.id).to.equal(stubValue.id);
+      expect(result.bookingRequest.id).to.equal(stubValue.id);
     });
 
     it("should return an error", async function() {
       sinon.restore();
       const logRepository = new LogRepository();
-      const repository = new CompanyRepository();
+      const repository = new BookingRequestRepository();
       var stub = sinon.stub(repository, "create").throwsException(); 
       var stubGeUser = sinon.stub(userRepository, "get").returns({ id: 1, });
       stub = sinon.stub(logRepository, "create").returns();
-      sinon.stub(repository, "getByRUT").returns(null);
-      const service = new CompanyService(repository, logRepository, userRepository);
+
+      const service = new BookingRequestService(repository, logRepository, userRepository);
       const result = await service.create(stubValue.id);
 
       expect(stub.calledOnce).to.be.true;
@@ -133,25 +93,25 @@ describe("CompanyService", function() {
     };
 
     it("should return a service group that matches the provided id", async function() {
-      const repository = new CompanyRepository();
+      const repository = new BookingRequestRepository();
       const stub = sinon.stub(repository, "get").returns(stubValue);
 
-      const service = new CompanyService(repository);
+      const service = new BookingRequestService(repository);
       const result = await service.get(stubValue.id);
 
       expect(stub.calledOnce).to.be.true;
-      expect(result.company.id).to.equal(stubValue.id);
-      expect(result.company.name).to.equal(stubValue.name);
+      expect(result.bookingRequest.id).to.equal(stubValue.id);
+      expect(result.bookingRequest.name).to.equal(stubValue.name);
     });
 
     it("should return an error", async function() {
       sinon.restore();
       const logRepository = new LogRepository();
-      const repository = new CompanyRepository();
+      const repository = new BookingRequestRepository();
       var stub = sinon.stub(repository, "get").throwsException(); 
       stub = sinon.stub(logRepository, "create").returns();
 
-      const service = new CompanyService(repository, logRepository);
+      const service = new BookingRequestService(repository, logRepository);
       const result = await service.get(stubValue.id);
 
       expect(stub.calledOnce).to.be.true;
@@ -166,25 +126,25 @@ describe("CompanyService", function() {
       name: 'Certificacion'
     };
 
-    it("should return a service group that matches the provided id", async function() {
-      const repository = new CompanyRepository();
+    it("should return a booking request that matches the provided id", async function() {
+      const repository = new BookingRequestRepository();
       const stub = sinon.stub(repository, "getByUser").returns([stubValue]);
 
-      const service = new CompanyService(repository);
+      const service = new BookingRequestService(repository);
       const result = await service.getByUser(stubValue.id);
 
       expect(stub.calledOnce).to.be.true;
-      expect(result.companies[0].id).to.equal(stubValue.id);
+      expect(result.bookingRequests[0].id).to.equal(stubValue.id);
     });
 
     it("should return an error", async function() {
       sinon.restore();
       const logRepository = new LogRepository();
-      const repository = new CompanyRepository();
+      const repository = new BookingRequestRepository();
       var stub = sinon.stub(repository, "getByUser").throwsException(); 
       stub = sinon.stub(logRepository, "create").returns();
 
-      const service = new CompanyService(repository, logRepository);
+      const service = new BookingRequestService(repository, logRepository);
       const result = await service.getByUser(stubValue.id);
 
       expect(stub.calledOnce).to.be.true;
@@ -200,24 +160,24 @@ describe("CompanyService", function() {
     };
 
     it("should return a service group that matches the provided id", async function() {
-      const repository = new CompanyRepository();
+      const repository = new BookingRequestRepository();
       const stub = sinon.stub(repository, "getAll").returns([stubValue]);
 
-      const service = new CompanyService(repository);
+      const service = new BookingRequestService(repository);
       const result = await service.getAll(stubValue.id);
 
       expect(stub.calledOnce).to.be.true;
-      expect(result.companies[0].id).to.equal(stubValue.id);
+      expect(result.bookingRequests[0].id).to.equal(stubValue.id);
     });
 
     it("should return an error", async function() {
       sinon.restore();
       const logRepository = new LogRepository();
-      const repository = new CompanyRepository();
+      const repository = new BookingRequestRepository();
       var stub = sinon.stub(repository, "getAll").throwsException(); 
       stub = sinon.stub(logRepository, "create").returns();
 
-      const service = new CompanyService(repository, logRepository);
+      const service = new BookingRequestService(repository, logRepository);
       const result = await service.getAll(stubValue.id);
 
       expect(stub.calledOnce).to.be.true;
