bf24afaa9750c988df01ccfe38e2bc0ba2633469
Romina Areco <romina.areco@hotmail.com>
2023-09-23 15:35:28 -0300

    Remove authentication

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,7 +34,7 @@ class UserController {
       const { linkEncrypt, password } = req.body; 
  
       if(!linkEncrypt || typeof linkEncrypt !== "string" || !password || typeof password !== "string") {
-        return res.status(401).json({ message: "Invalid Params" });
+        return res.status(400).json({ message: "Invalid Params" });
       }
       const result = await this.service.updatePassword(linkEncrypt, password);
       return res.status(200).json(result);
